bd29e8901ca9dde654187421829b1bfc73b59824
Matt Dodge <mattedgod@gmail.com>
2012-11-20 11:31:41 -0800

    Counter Pod can take different images/sizes now

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -5,49 +5,55 @@ EasyDash.availablePods.CounterPod = EasyDash.DashPod.extend({
 	defaults : function() {
 		return $.extend({}, {
 			animationDuration : 215,
-			minDigits : 0
+			minDigits : 0,
+			digitsImg: 'EasyDash/img/digits.png',
+			fw : 53,
+			tfh : 39,
+			bfh : 64,
+			boff : 390
 		},
 		this.constructor.__super__.defaults);
 	},
 	
 	podPostwork : function(pod) {
-		var me = this;
+		var me = this,
+		idString = me.get("podId")+'-counter';
 		
 		var podDiv = $('<div/>')
 			.addClass('flip-counter')
-			.attr('id',me.get("podId")+'-counter');
+			.attr('id',idString);
 			
 		var podLabel = $('<div/>')
 			.addClass('counter-label')
 			.html(me.get("podTitle"));
 			
 		$(pod).append(podDiv).append(podLabel);
-			
+		
+		fw = me.get("fw"),
+		tfh = me.get("tfh"),
+		bfh = me.get("bfh"),
+		boff = me.get("boff");
+		
 		var countObj = new flipCounter(podDiv.attr('id'), {
 			auto: false,
 			pace: 2000,
 			minDigits : me.get("minDigits"),
-//			fW : 36, // was 53
-//			tFH : 26, // was 39
-//			bFH : 43, // was 64
-//			bOffset : 265 // was 390
-			fW : 53, // was 53
-			tFH : 39, // was 39
-			bFH : 64, // was 64
-			bOffset : 390 // was 390
+			fW : fw, 
+			tFH : tfh,
+			bFH : bfh,
+			bOffset : boff
 		});
 
+		
+		
 		var css = document.createElement("style");
 		css.type = "text/css";
 		css.innerHTML = 
-			".flip-counter ul.cd{float:left;list-style-type:none;margin:0;padding:0}" +
-			".flip-counter li{background:url('EasyDash/img/digits.png') 0 0 no-repeat}" +
-//			".flip-counter li.t{background-position:0 0;width:36px;height:26px}" +
-//			".flip-counter li.b{background-position:0 0;width:36px;height:43px}" +
-//			".flip-counter li.s{background-position:-37px -719px;width:10px;height:69px}";
-			".flip-counter li.t{background-position:0 0;width:53px;height:39px}" +
-			".flip-counter li.b{background-position:0 0;width:53px;height:64px}" +
-			".flip-counter li.s{background-position:-53px -1030px;width:14px;height:103px}";
+			"#"+idString+" ul.cd{float:left;list-style-type:none;margin:0;padding:0}" +
+			"#"+idString+" li{background:url('" + me.get("digitsImg") + "') 0 0 no-repeat}" +
+			"#"+idString+" li.t{background-position:0 0;width:" + fw + "px;height:" + tfh + "px}" +
+			"#"+idString+" li.b{background-position:0 0;width:" + fw + "px;height:" + bfh + "px}" +
+			"#"+idString+" li.s{background-position:-" + fw + "px -" + (bfh * 10 + boff) + "px;width:" + (fw * 14 / 53) + "px;height:" + (tfh + bfh) + "px}";
 			
 		document.head.appendChild(css);
 		
@@ -69,7 +75,6 @@ EasyDash.availablePods.CounterPod = EasyDash.DashPod.extend({
 		} else {
 			countObj.incrementTo(
 				parseInt(data["count"]),
-	//			12345,
 				me.get("refreshInterval")/1000,
 				me.get("animationDuration")
 			);
