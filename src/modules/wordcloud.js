8320bda0a0e982c61f99b3e8414142d9ef210659
Matt Dodge <mattedgod@gmail.com>
2012-11-20 13:34:15 -0800

    WordCloud showLoading

--- src/modules/wordcloud.js
+++ src/modules/wordcloud.js
@@ -29,6 +29,28 @@ EasyDash.availablePods.WordCloudPod = EasyDash.DashPod.extend({
 		canvas.wordCloud($.extend(true, {
 			wordList: data
 		}, me.get("wordCloudParams")));
+	},
+	
+	showLoading : function(str) {
+		if (!str) str = "Generating word cloud...";
+		var me = this,
+			canvas = me.get("canvas"),
+			ctx = canvas[0].getContext('2d');
+		
+		// Store the current transformation matrix
+		ctx.save();
+		
+		// Use the identity matrix while clearing the canvas
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		
+		// Restore the transform
+		ctx.restore();
+		
+		ctx.fillStyle = '#333';
+		ctx.font = "italic bold 12px 'Lucida Grande', 'Lucida Sans Unicode', Helvetica, Arial, Verdana, sans-serif";
+		ctx.textBaseline = 'bottom';
+		ctx.fillText(str, 5, 25);
 	}
 	
 });
